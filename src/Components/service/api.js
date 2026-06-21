230ed7cf73cbecb73947cd8f8dec9055538b9db5
CHINTAM SAI PRAKASH REDDY <128221894+Csaiprakash@users.noreply.github.com>
2026-06-21 11:45:05 +0530

    Update api.js

--- src/Components/service/api.js
+++ src/Components/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "https://movie-review-backend-3wee.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
